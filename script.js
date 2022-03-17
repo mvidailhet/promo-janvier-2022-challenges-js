a2cd2f4b74ed2b38d394fc56ee0da76f5bf74191
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-03-17 16:29:02 +0100

    Challenge11

--- script.js
+++ script.js
@@ -1,49 +1,33 @@
-function calcTip(note) {
-  const tipPercent = getTipPercent(note);
-  return note * tipPercent;
+const fixedValue = 1;
+
+const calcBMI = function() {
+  this.BMI = this.weight / (this.height * this.height);
+  this.fixedBMI = this.BMI.toFixed(fixedValue);
+  return this.BMI;
+};
+
+const user1 = {
+  name: "Bernard",
+  height: 1.69,
+  weight: 78,
+  calcBMI: calcBMI,
+};
+
+const user2 = {
+  name: "Marcel",
+  height: 1.95,
+  weight: 92,
+  calcBMI: calcBMI,
+};
+
+user1.calcBMI();
+user2.calcBMI();
+
+console.log(user1);
+console.log(user2);
+
+if (user1.BMI > user2.BMI) {
+  console.log(`L'imc de ${user1.name} (${user1.fixedBMI}) est plus élevé que l'IMC de ${user2.name} (${user2.fixedBMI})`);
+} else {
+  console.log(`L'imc de ${user2.name} (${user2.fixedBMI}) est plus élevé que l'IMC de ${user1.name} (${user1.fixedBMI})`);
 }
-
-function getTipPercent(note) {
-  return note > 50 && note < 300 ? 0.15 : 0.2;
-}
-
-const getTipPercentArrow = (note) => (note > 50 && note < 300 ? 0.15 : 0.2);
-
-const notes = [125, 555, 44];
-
-let tipsFor = [];
-for (let i = 0; i < notes.length; i++) {
-  const total = calcTip(notes[i]);
-  tipsFor.push(total);
-}
-
-let tipsForeach = [];
-notes.forEach((note) => {
-  const total = calcTip(note);
-  tipsForeach.push(total);
-});
-
-let tipsWhile = [];
-let index = 0;
-while (index < notes.length) {
-  const total = calcTip(notes[index]);
-  tipsWhile.push(total);
-  index += 1;
-}
-
-const tipsMap = notes.map((note) => calcTip(note));
-
-/* console.log(tipsFor);
-console.log(tipsForeach);
-console.log(tipsWhile);
-console.log(tipsMap); */
-
-let totals = [];
-for (let i = 0; i < notes.length; i++) {
-  const total = notes[i] + tipsFor[i];
-  totals.push(total);
-}
-
-console.log(totals);
-
-
